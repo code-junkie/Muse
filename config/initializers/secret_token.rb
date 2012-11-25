3967453ed3b82b1aaddc5ce706225bc61a0be073
code-junkie <contact@code-junkie.com>
2012-11-24 23:40:22 -0700

    Adding the  basic Rails file structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Muse::Application.config.secret_token = '3b8f8ceb0ed4c068f90f8acd2e4e8bb5e184d6c3b0f0158dcd3d98309911d3f5c6bb04dd605d2d81138a9ce0b2e1a6b75aaa0a9e1d4726ba74b0aa25647e4bbd'
